056813e09ccb2f76a2350c855d0d600d80eec3c2
Guillermo Rossi <guillemaartin.gr@gmail.com>
2023-04-05 11:23:58 -0300

    ./javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,32 +1,32 @@
-// const fetch_URL = ("./javascript/train.json")
+const fetch_URL = ("./javascript/train.json")
 
-// const contenedor = document.getElementById("contenedor")
+const contenedor = document.getElementById("contenedor")
 
-// function loading(){
-//     fetch(fetch_URL)
+function loading(){
+    fetch(fetch_URL)
 
-//     .then(function(res){
-//         return(res.json());
-//     })
+    .then(function(res){
+        return(res.json());
+    })
 
 
-//     .then(function(pages){
-//        let html = '';
+    .then(function(pages){
+       let html = '';
 
-//         pages.map((page) => {
-//             html += 
-//              `  <div class="asideContenedores" style="background-color:${page.backcolor};">
-//                     <p>${page.number}</p>
-//                     <h1>${page.asesoria}</h1> <br>
-//                     </div>
-//                 `
-//            });
+        pages.map((page) => {
+            html += 
+             `  <div class="asideContenedores" style="background-color:${page.backcolor};">
+                    <p>${page.number}</p>
+                    <h1>${page.asesoria}</h1> <br>
+                    </div>
+                `
+           });
 
-//         contenedor.innerHTML = html;
-//     })    
-//     .catch(err => {
-//         console.log(err)
-//     })
-// }
+        contenedor.innerHTML = html;
+    })    
+    .catch(err => {
+        console.log(err)
+    })
+}
 
-// loading()
+loading()
